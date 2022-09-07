da41f041abd196d7f3d50ea6c7f85dd04b78d5b1
mpfis <max.fisher@databricks.com>
2022-09-07 01:37:57 -0500

    final update

--- main.py
+++ main.py
@@ -2,4 +2,5 @@
 df = spark.range(1000)
 df = df.selectExpr("id", "(id * 1000) as pid")
 df = df.selectExpr("id", "pid", "(pid * 2) as pid2")
+df = df.selectExpr("id", "pid", "pid2", "(pid/pid2) as quotient")
 df.head(10)
