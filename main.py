336198f2e8cd212dbf14526af3f40e0173da84fa
mpfis <max.fisher@databricks.com>
2022-09-07 12:48:12 -0500

    added Hi Jess and tested dbutils

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Databricks notebook source
+from pyspark.sql.functions import *
 df = spark.range(1000)
 df = df.selectExpr("id", "(id * 1000) as pid")
 df = df.selectExpr("id", "pid", "(pid * 2) as pid2")
 df = df.selectExpr("id", "pid", "pid2", "(pid/pid2) as quotient")
+print("hello")
+print("goodbye")
+print("Hi Jess!")
+df2 = df.withColumn("id3", col("id")*3)
 df.head(10)
+
+dbutils.fs.ls("/")
